9385eb27f1b4c251e56c423af91459734d074b2c
Pinki Sharma <crabs962510@gmail.com>
2026-01-16 01:10:56 +0530

    Fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-})
+  base: "/", // 👈 🔥 THIS LINE FIXES EVERYTHING
+  plugins: [react(), tailwindcss()],
+});
